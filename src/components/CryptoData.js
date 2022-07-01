ccee1db7d352a9c6d98013e6c94d79cc2a37f4c0
TXT-46 <txt-46@TXT-46.attlocal.net>
2022-06-30 17:08:21 -0700

    third-party-apis-challenge

--- src/components/CryptoData.js
+++ src/components/CryptoData.js
@@ -1,31 +1,37 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 
-const CryptoData = () => {
+const CryptoData2 = () => {
     // data: represents our data, setdata sets the data we are pulling from the api
     const [data, setData] = useState([]);
-    // collect user data, empty string
-    const baseUrl = "https://www.cryptingup.com/api";
-    
-    // Make a request to all exchanges
+    const [data2, setData2] = useState([]);
+    const [data3, setData3] = useState([]);
+
+     // Make a request to all exchanges
     useEffect(() => {
-        axios.get(`${baseUrl}/exchanges`).then(res => {
-            setData(res.data.exchanges);
+        axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+            setData(res.data.markets);
         });
+        axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+            setData2(res.data.markets);
+    });
+    axios.get('https://www.cryptingup.com/api/exchanges/COINBASE/markets?size=10&start=1&end=1').then(res => {
+        setData3(res.data.markets);
+});
     }, []);
    
 
     return (
         <div>
             <h1>Data from my API</h1>
-            {data.map(x => <li key={x.exchange_id}>{x.name}</li>)}
-            
+        <h2>Prices:    {data3.map((x, i) => <li key={i}>{x.price} </li>)}</h2>
+            <h2> symbol:{data.map((x, i) => <li key={i}>{x.symbol} </li>)}</h2>
+            <h2> Exchange</h2> {data2.map((x, i) => <li key={i}>{x.exchange_id} </li>)}
             {/* List specific data for the 3 exchanges  */}
-            <h1>Single Post data</h1>
-            <h3>Exchange Name: </h3>
+           
             <p> </p>
         </div>  
     )
 };
 
-export default CryptoData;
+export default CryptoData2;
